faf21e190d9d5fd83e476c3f6d5557c53486d7db
AJoshna02 <anangijoshna@gmail.com>
2026-05-10 20:15:49 +0530

    fix: replace passlib with bcrypt directly

--- utils/hashing.py
+++ utils/hashing.py
@@ -1,9 +1,10 @@
-from passlib.context import CryptContext
+import bcrypt
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def get_password_hash(password: str) -> str:
+    pwd_bytes = password[:72].encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    pwd_bytes = plain_password[:72].encode('utf-8')
+    return bcrypt.checkpw(pwd_bytes, hashed_password.encode('utf-8'))
