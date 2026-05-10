89d5fd3b96b98d4d1717a0fac313ee482e0cdb4b
AJoshna02 <anangijoshna@gmail.com>
2026-05-10 20:04:52 +0530

    fix: connect to PostgreSQL

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./parking.db"  # or your actual DB URL
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./parking.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# This is what's missing!
 def get_db():
     db = SessionLocal()
     try:
